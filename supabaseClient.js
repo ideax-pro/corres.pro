ea706a8dc628cb1c42f9a3e55c1230732f3ae703
ideax-pro <maratonmoroleon27@gmail.com>
2026-08-30 18:14:48 -0600

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,8 @@
+// Configuración de credenciales de Supabase
+const SUPABASE_URL = 'https://gwfujkcfpugzgqqrsivg.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3ZnVqa2NmcHVnemdxcXJzaXZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxMTQ1ODUsImV4cCI6MjEwMzY5MDU4NX0.Dy28mWiMGTRWZwK6l33Ch3jeq5ZaLqJr86p6sfZlHhc';
+
+// Inicialización del cliente Supabase
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+console.log('✅ Supabase conectado exitosamente.');
